536499e2a6a1a74076b110ba5dee4cec22daee4a
kaboreibrahim <147706870+kaboreibrahim@users.noreply.github.com>
2026-07-06 11:15:56 +0000

    first commit

--- apps/dashboard/context_processors.py
+++ apps/dashboard/context_processors.py
@@ -1,5 +1,5 @@
 def dashboard_globals(request):
-    if request.user.is_authenticated and request.user.is_staff:
+    if hasattr(request, 'user') and request.user.is_authenticated and request.user.is_staff:
         from apps.website.models import Contact
         return {'sidebar_messages_non_lus': Contact.objects.filter(lu=False).count()}
     return {}
